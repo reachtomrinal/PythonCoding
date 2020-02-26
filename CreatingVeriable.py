fe071c2dcb19ed00ae77860c956353da6c47e6f9
Mrinal Kanti Kumar <61320886+reachtomrinal@users.noreply.github.com>
2020-02-26 15:53:30 +0000

    Add files via upload

---
+++ CreatingVeriable.py
@@ -0,0 +1,48 @@
+#Section 1.2: Creating variables and assigning values
+# Integer
+a=2
+print(a)
+# Output: 2
+
+# Integer
+b = 9223372036854775807
+print(b)
+# Output: 9223372036854775807
+
+# Floating point
+pi = 3.14
+print(pi)
+# Output: 3.14
+
+# String
+c = 'A'
+print(c)
+# Output: A
+
+# String
+name = 'John Doe'
+print(name)
+# Output: John Doe
+
+# Boolean
+q = True
+print(q)
+# Output: True
+
+# Empty value or null data type
+x = None
+print(x)
+# Output: None
+
+#list of keywords
+import keyword
+print(keyword.kwlist)
+
+q = True
+print(type(q))
+# Output: <type 'bool'>
+
+
+x = None
+print(type(x))
+# Output: <type 'NoneType'>
---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+#This is for print
+print ("hello, world")
+
+#This is for run a file interactively
+#python -i HelloWorld.py
+
+#run command AS A STRING
+#python -c 'print("Run command as a string")'
+
