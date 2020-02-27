33f086144b354a85577c424629f23b28b516e94f
Mrinal Kanti Kumar <61320886+reachtomrinal@users.noreply.github.com>
2020-02-27 16:48:38 +0000

    Add files via upload
    uploaded list and user inputfile

---
+++ GettingInputFromuser.py
@@ -0,0 +1,8 @@
+name = input("enter your name: ")
+print("Hello " + name + "!")
+
+#sample calculator
+num1 = input("Enter 1st number: ")
+num2 = input("Enter 2nd Number: ")
+print(float(num1) + float(num2))
+
---
+++ MathFunction.py
@@ -0,0 +1,7 @@
+from math import floor
+
+print(pow(4, 6))
+print(min(4, 8))
+
+#floor is the lower alue
+print(floor(3.5))
